2bbf0669de47e131fcb9b35bd72264bbe13cf1aa
Fantamomo <me@fantamomo.com>
2025-04-04 14:28:32 +0200

    Delete simple script

--- src/main.py
+++ src/main.py
@@ -1,21 +1,3 @@
-"""
-This script shows a simple scripted flight path using the MotionCommander class.
-
-Simple example that connects to the crazyflie at `URI` and runs a
-sequence. Change the URI variable to your Crazyflie configuration.
-"""
-import logging
-import time
-
-import cflib.crtp
-from cflib.crazyflie.syncCrazyflie import SyncCrazyflie
-from cflib.positioning.motion_commander import MotionCommander
-
-URI = 'radio://0/80/2M'
-
-# Only output errors from the logging framework
-logging.basicConfig(level=logging.ERROR)
-
 
 if __name__ == '__main__':
     # Initialize the low-level drivers (don't list the debug drivers)
